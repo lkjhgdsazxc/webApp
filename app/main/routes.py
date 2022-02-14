59cd9d9903c76590fea7a7a124c1464b71f9e684
root <lkjhgdsazxc@gmail.com>
2022-02-14 09:36:53 +0000

    FOR Demo

--- app/main/routes.py
+++ app/main/routes.py
@@ -36,6 +36,28 @@ def index():
     return render_template('index.html', title=_('Home'), form=form,
                            posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
+                           
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/Community', methods=['GET', 'POST'])
+@login_required
+def Community():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash(_('Your post is now live!'))
+        return redirect(url_for('main.Community'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.Community', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main.Community', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('Community.html', title=_('Community'), form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/explore')
