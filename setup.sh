b1f3390e24f2474c3c660027a91da035e7562ef3
dickylam <190157802@stu.vtc.edu.hk>
2022-03-29 11:02:15 +0000

    add auto start supervisod

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 . venv/bin/activate
 pip install -r requirements.txt
 chmod 711 /home/ec2-user
+sudo cp /home/ec2-user/environment/webApp/supervisord.service /etc/systemd/system/
+sudo systemctl start supervisord.service
 supervisord -c gunicorn.conf 
 sudo yum install -y nginx
 sudo systemctl enable nginx
