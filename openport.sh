877c813c4057e0c4b77e249b2ab679aa83f2afdb
dickylam <190157802@stu.vtc.edu.hk>
2022-03-29 08:26:51 +0000

    supervisor

--- openport.sh
+++ openport.sh
@@ -2,4 +2,4 @@ security_group=$(ec2-metadata -s | cut -d " " -f 2);
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 443 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8080 --cidr 0.0.0.0/0
-
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 9001 --cidr 0.0.0.0/0
