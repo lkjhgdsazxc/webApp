dce220cba890e929c1a44876aa22aa63e2932391
dickylam <190157802@stu.vtc.edu.hk>
2022-03-29 09:50:01 +0000

    a setup file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+. venv/bin/activate
+pip install -r requirements.txt
+chmod 711 /home/ec2-user
+supervisord -c gunicorn.conf 
+sudo yum install -y nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+rm /etc/nginx/nginx.conf
+cp /home/ec2-user/environment/webApp/nginx.conf /etc/nginx/
+sudo nginx -t
+sudo service nginx restart
+sudo systemctl status nginx
