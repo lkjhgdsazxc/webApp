8c802c8b3795d9094197c30db0db626f9d0c739b
lkjhgdsazxc <55237882+lkjhgdsazxc@users.noreply.github.com>
2022-05-02 18:44:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:rootroot@fyp-db.c5ytzphcj872.us-east-1.rds.amazonaws.com/FYP_DATABASE'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:rootroot@fyp-database.czk7gbc2rlgh.us-east-1.rds.amazonaws.com/FYP_DATABASE'
     RECAPTCHA_PUBLIC_KEY = "6Le_caofAAAAAGHbz5L255xGnwEFc7XkD2xcapi8"
     RECAPTCHA_PRIVATE_KEY = "6Le_caofAAAAAJRQPmJXFMmoimOJmiCZh8XWaIR3"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
