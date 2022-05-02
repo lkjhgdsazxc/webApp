c7c1c82806e66b6626516738a054013e0966b3f4
EC2 Default User <ec2-user@ip-172-31-21-30.ec2.internal>
2022-05-02 08:58:50 +0000

    Final ver 2.1

---
+++ FYPstore/store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+
+admin.autodiscover()
+
+urlpatterns = [
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+    path('update_Item/', views.updateItem, name="update_Item"),
+    path('process_order/', views.processOrder, name="process_order"),
+]
+
